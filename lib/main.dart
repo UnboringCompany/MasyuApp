2650d4b6acba71e6edec02390b7bd70cf4059923
Léo WADIN <46723431+ArKc0s@users.noreply.github.com>
2023-03-31 10:19:01 +0200

    FEAT: Ajout du bouton des règles et des paramètres

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -33,62 +33,89 @@ class MyApp extends StatelessWidget {
 
 class MenuPage extends StatefulWidget {
   const MenuPage({Key? key}) : super(key: key);
-  
+
   @override
   State<StatefulWidget> createState() => _MenuState();
 }
 
 class _MenuState extends State<MenuPage> {
-
   //TODO: Changer les tailles de sizedbox en dynamique
 
   @override
   Widget build(BuildContext context) {
-
     final size = MediaQuery.of(context).size;
     String _selectedOption = 'Option 1';
     List<String> _options = ['Option 1', 'Option 2', 'Option 3'];
 
     return CoreWidget(
-      child: Center(child : Column(
+        child: Center(
+      child: Column(
         children: [
           const SizedBox(height: 100),
-          const Text("MASYU", style: TextStyle(color: Colors.white, letterSpacing: 10, fontSize: 40, fontWeight: FontWeight.w600)),
+          const Text("MASYU",
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 10,
+                  fontSize: 40,
+                  fontWeight: FontWeight.w600)),
           CitationWidget(),
           const SizedBox(height: 80),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [
-              Tile(icon: Icon(BootstrapIcons.play, color: Color(0xff3D4AEB), size: 100,), title: "Reprendre\n6x6 - 1min37"),
-              Tile(icon: Icon(BootstrapIcons.watch, color: Color(0xff3D4AEB), size: 80), title: "Défi\nContre la montre")
-          ]),
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                Tile(
+                    icon: Icon(
+                      BootstrapIcons.play,
+                      color: Color(0xff3D4AEB),
+                      size: 100,
+                    ),
+                    title: "Reprendre\n6x6 - 1min37"),
+                Tile(
+                    icon: Icon(BootstrapIcons.watch,
+                        color: Color(0xff3D4AEB), size: 80),
+                    title: "Défi\nContre la montre")
+              ]),
           const SizedBox(height: 80),
           Container(
             width: 0.85 * size.width,
             height: 60,
             decoration: BoxDecoration(
-              color: Color(0xff3D4AEB),
-              borderRadius: BorderRadius.circular(10)
-            ),
+                color: Color(0xff3D4AEB),
+                borderRadius: BorderRadius.circular(10)),
             child: ElevatedButton(
-              onPressed: () {
-                
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.transparent,
-                elevation: 0,
-              ),
-              child: Text("Nouvelle partie")),
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.transparent,
+                  elevation: 0,
+                ),
+                child: Text("Nouvelle partie")),
           ),
           const SizedBox(height: 15),
-          const GridSizeMenu()
+          const GridSizeMenu(),
+          const SizedBox(height: 70),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                width: 0.3 * size.width,
+                height: 40,
+                decoration: BoxDecoration(
+                    color: Color(0xffB15653),
+                    borderRadius: BorderRadius.circular(10)),
+                child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.transparent,
+                      elevation: 0,
+                    ),
+                    child: Text("Règles")),
+              ),
+              SizedBox(width: 0.01 * size.width),
+              IconButton(onPressed: () {}, icon: const Icon(BootstrapIcons.gear, color: Colors.white,))
+            ],
+          )
         ],
       ),
     ));
   }
-
 }
-
-
-
-
