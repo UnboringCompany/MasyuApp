35ff46f38a8beba069f860f60d1635cafa93b29d
Damien-Collot <dcollot76@gmail.com>
2023-03-31 12:22:38 +0200

    Feat : génération d'une grille 6x6 avec sa solution

--- lib/objects/grille.dart
+++ lib/objects/grille.dart
@@ -1,3 +1,7 @@
+import 'dart:math';
+
+import 'package:masyu_app/objects/cercle.dart';
+
 import 'cell.dart';
 import 'trait.dart';
 
@@ -6,14 +10,16 @@ class Grille {
   int size;
   List<Cell> listeCells;
   List<Trait> listeTraits;
+  List<Trait> listeSolution = List<Trait>.empty(growable: true);
+  int choice = 0;
 
   // Constructeur
-    Grille(this.size)
+  Grille(this.size)
       : listeCells = List<Cell>.empty(growable: true),
         listeTraits = List<Trait>.empty(growable: true);
 
   // Méthodes
-  void addCase(Cell c) {
+  void addCell(Cell c) {
     listeCells.add(c);
   }
 
@@ -29,17 +35,84 @@ class Grille {
     // TODO
   }
 
-  void generate() {
-    // TODO
+  void generateCells() {
     for (int i = 0; i < size; i++) {
       for (int j = 0; j < size; j++) {
-        addCase(Cell(i, j));
+        addCell(Cell(i, j));
       }
     }
-    int nb_cercles_blancs = size * size ~/ 2;
-    int nb_cercles_noirs = size * size - nb_cercles_blancs;
+  }
 
+  Cell getCell(int x, int y) {
+    for (Cell c in listeCells) {
+      if (c.posX == x && c.posY == y) {
+        return c;
+      }
+    }
+    return Cell(0, 0);
+  }
 
+  void replaceCell(int x, int y, int color) {
+    for (int i = 0; i < size; i++) {
+      if (listeCells[i].posX == x && listeCells[i].posY == y) {
+        listeCells[i] = Cercle(x, y, color, false);
+        break;
+      }
+    }
+  }
+
+  void generateGrille() {
+    choice = 0;
+    generateCells();
+    switch (choice) {
+      case 0:
+        replaceCell(0, 1, 1);
+        replaceCell(1, 5, 1);
+        replaceCell(2, 1, 1);
+        replaceCell(2, 2, 1);
+        replaceCell(2, 3, 1);
+        replaceCell(3, 3, 2);
+        replaceCell(4, 0, 1);
+        replaceCell(4, 2, 2);
+        replaceCell(5, 3, 1);
+        listeSolution.add(Trait(getCell(0, 0), getCell(0, 1)));
+        listeSolution.add(Trait(getCell(0, 1), getCell(0, 2)));
+        listeSolution.add(Trait(getCell(0, 2), getCell(0, 3)));
+        listeSolution.add(Trait(getCell(0, 3), getCell(0, 4)));
+        listeSolution.add(Trait(getCell(0, 4), getCell(0, 5)));
+        listeSolution.add(Trait(getCell(0, 5), getCell(1, 5)));
+        listeSolution.add(Trait(getCell(1, 5), getCell(2, 5)));
+        listeSolution.add(Trait(getCell(2, 5), getCell(3, 5)));
+        listeSolution.add(Trait(getCell(3, 5), getCell(3, 4)));
+        listeSolution.add(Trait(getCell(3, 4), getCell(3, 3)));
+        listeSolution.add(Trait(getCell(3, 3), getCell(2, 3)));
+        listeSolution.add(Trait(getCell(2, 3), getCell(1, 3)));
+        listeSolution.add(Trait(getCell(1, 3), getCell(1, 2)));
+        listeSolution.add(Trait(getCell(1, 2), getCell(2, 2)));
+        listeSolution.add(Trait(getCell(2, 2), getCell(3, 2)));
+        listeSolution.add(Trait(getCell(3, 2), getCell(4, 2)));
+        listeSolution.add(Trait(getCell(4, 2), getCell(4, 3)));
+        listeSolution.add(Trait(getCell(4, 3), getCell(4, 4)));
+        listeSolution.add(Trait(getCell(4, 4), getCell(5, 4)));
+        listeSolution.add(Trait(getCell(5, 4), getCell(5, 3)));
+        listeSolution.add(Trait(getCell(5, 3), getCell(5, 2)));
+        listeSolution.add(Trait(getCell(5, 2), getCell(5, 1)));
+        listeSolution.add(Trait(getCell(5, 1), getCell(5, 0)));
+        listeSolution.add(Trait(getCell(5, 0), getCell(4, 0)));
+        listeSolution.add(Trait(getCell(4, 0), getCell(3, 0)));
+        listeSolution.add(Trait(getCell(3, 0), getCell(3, 1)));
+        listeSolution.add(Trait(getCell(3, 1), getCell(2, 1)));
+        listeSolution.add(Trait(getCell(2, 1), getCell(1, 1)));
+        listeSolution.add(Trait(getCell(1, 1), getCell(1, 0)));
+        listeSolution.add(Trait(getCell(1, 0), getCell(0, 0)));
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+    }
   }
 
   void solve() {
@@ -57,7 +130,7 @@ class Grille {
   }
 
   void printGrid() {
-    generate();
+    generateGrille();
     print(listeCells.toString());
   }
 }
