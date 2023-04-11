0a7acd92df9b02256dee7b1e4c95bcca144339ee
Damien-Collot <dcollot76@gmail.com>
2023-04-11 10:55:46 +0200

    Feat : fin de la page settings.dart

--- lib/setings.dart
+++ lib/setings.dart
@@ -1,5 +1,6 @@
 import 'package:bootstrap_icons/bootstrap_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_volume_controller/flutter_volume_controller.dart';
 import 'package:masyu_app/widgets/core.dart';
 import 'package:masyu_app/widgets/tile.dart';
 import 'main.dart';
@@ -12,6 +13,15 @@ class Settings extends StatefulWidget {
 }
 
 class _Settings extends State<Settings> {
+  bool? sound = true;
+  bool vibrate = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getMuteState();
+  }
+
   void seeHomePage() {
     Navigator.pushReplacement<void, void>(
       context,
@@ -21,11 +31,25 @@ class _Settings extends State<Settings> {
     );
   }
 
+  Future<void> getMuteState() async {
+    bool? muted = await FlutterVolumeController.getMute();
+    setState(() {
+      sound = !muted!;
+    });
+  }
+
+  Future<void> onSoundChanged(bool value) async {
+    await FlutterVolumeController.setMute(!value);
+    setState(() {
+      sound = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    String _selectedOption = 'Option 1';
-    List<String> _options = ['Option 1', 'Option 2', 'Option 3'];
+    String _selectedOption = 'Français';
+    List<String> _options = ['Français', 'Deutch', 'English', 'Español', '日本'];
 
     return CoreWidget(
         child: Center(
@@ -40,6 +64,7 @@ class _Settings extends State<Settings> {
                   color: Colors.white,
                   size: 25,
                 )),
+            const SizedBox(width: 15),
             const Text("MASYU",
                 style: TextStyle(
                     color: Colors.white,
@@ -48,8 +73,106 @@ class _Settings extends State<Settings> {
                     fontWeight: FontWeight.w600))
           ]),
           const SizedBox(height: 80),
-          const SizedBox(height: 15),
-          const SizedBox(height: 70),
+          Container(
+            width: 125,
+            height: 48,
+            decoration: BoxDecoration(
+                color: Color.fromARGB(50, 217, 217, 217),
+                border: Border.all(
+                  color: Color.fromARGB(50, 217, 217, 217),
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  BootstrapIcons.volume_up_fill,
+                  color: Colors.white,
+                  size: 30,
+                ),
+                const SizedBox(width: 15),
+                Switch(
+                  value: sound ?? true,
+                  onChanged: onSoundChanged,
+                  activeColor: Colors.white,
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 40),
+          Container(
+            width: 125,
+            height: 48,
+            decoration: BoxDecoration(
+                color: Color.fromARGB(50, 217, 217, 217),
+                border: Border.all(
+                  color: Color.fromARGB(50, 217, 217, 217),
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  BootstrapIcons.phone_vibrate,
+                  color: Colors.white,
+                  size: 30,
+                ),
+                const SizedBox(width: 15),
+                Switch(
+                  value: vibrate,
+                  onChanged: (bool value) {
+                    // This is called when the user toggles the switch.
+                    setState(() {
+                      vibrate = value;
+                    });
+                  },
+                  activeColor: Colors.white,
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 50),
+          Container(
+            width: 220,
+            height: 49,
+            decoration: BoxDecoration(
+              color: Color(0xff373855),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Center(
+              child: Theme(
+                data: ThemeData(
+                  canvasColor: Color(0xff373855),
+                ),
+                child: DropdownButton<String>(
+                  underline: SizedBox(),
+                  value: _selectedOption,
+                  iconEnabledColor: Colors.white,
+                  style: const TextStyle(color: Colors.white),
+                  onChanged: (String? value) {
+                    setState(() {
+                      _selectedOption = value!;
+                    });
+                  },
+                  items: _options.map((String option) {
+                    return DropdownMenuItem<String>(
+                      value: option,
+                      child: SizedBox(
+                        width: 220 - 24,
+                        child: Text(
+                          option,
+                          textAlign: TextAlign.center,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 60),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import cloud_firestore
 import firebase_core
+import flutter_volume_controller
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FlutterVolumeControllerPlugin.register(with: registry.registrar(forPlugin: "FlutterVolumeControllerPlugin"))
 }
