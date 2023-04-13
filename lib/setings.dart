5dd40c9556c19205b588f096edb4bf24dbbc77e7
Damien-Collot <dcollot76@gmail.com>
2023-04-13 12:02:03 +0200

    Feat : modification du pseudo

--- lib/setings.dart
+++ lib/setings.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:bootstrap_icons/bootstrap_icons.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_volume_controller/flutter_volume_controller.dart';
 import 'package:masyu_app/appstate.dart';
@@ -21,11 +26,49 @@ class _Settings extends State<Settings> {
   bool vibrate = true;
   String _selectedOption = 'Français';
   List<String> _options = ['English', 'Deutch', 'Français', 'Español', '日本'];
+  String pseudo = "";
+  bool newJoueur = true;
 
   @override
   void initState() {
+    _getPseudo();
     super.initState();
     getLocal();
+  }
+
+  Future<String?> _getId() async {
+    var deviceInfo = DeviceInfoPlugin();
+    if (Platform.isIOS) {
+      final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+      var data = await deviceInfoPlugin.iosInfo;
+      var identifier = data.identifierForVendor;
+      return identifier;
+    } else if (Platform.isAndroid) {
+      final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+      AndroidDeviceInfo androidInfo;
+      try {
+        androidInfo = await deviceInfo.androidInfo;
+        return androidInfo.id;
+      } catch (e) {
+        print('Error: $e');
+      }
+    }
+    return null;
+  }
+
+  Future<void> _getPseudo() async {
+    String? id = await _getId();
+    await Firebase.initializeApp();
+    DocumentSnapshot<Map<String, dynamic>> documentSnapshot =
+        await FirebaseFirestore.instance
+            .collection('utilisateur')
+            .doc(id)
+            .get();
+    if (documentSnapshot.exists) {
+      setState(() {
+        pseudo = documentSnapshot.data()?['pseudo'];
+      });
+    }
   }
 
   void getLocal() {
@@ -48,13 +91,12 @@ class _Settings extends State<Settings> {
     Navigator.of(context).pushNamed('/');
   }
 
-
   final List locale = [
-    {'name': 'Français', 'locale': Locale('fr', 'FR')},
-    {'name': 'English', 'locale': Locale('en', 'US')},
-    {'name': 'Deutch', 'locale': Locale('de', 'DE')},
-    {'name': 'Español', 'locale': Locale('es', 'ES')},
-    {'name': '日本', 'locale': Locale('ja', 'JP')},
+    {'name': 'Français', 'locale': const Locale('fr', 'FR')},
+    {'name': 'English', 'locale': const Locale('en', 'US')},
+    {'name': 'Deutch', 'locale': const Locale('de', 'DE')},
+    {'name': 'Español', 'locale': const Locale('es', 'ES')},
+    {'name': '日本', 'locale': const Locale('ja', 'JP')},
   ];
 
   updatelanguage(Locale locale) {
@@ -75,14 +117,14 @@ class _Settings extends State<Settings> {
           Row(mainAxisAlignment: MainAxisAlignment.center, children: [
             IconButton(
                 onPressed: seeHomePage,
-                icon: Icon(
+                icon: const Icon(
                   BootstrapIcons.arrow_left,
                   color: Colors.white,
                   size: 25,
                 )),
             const SizedBox(width: 15),
             Text('title'.tr,
-                style: TextStyle(
+                style: const TextStyle(
                     color: Colors.white,
                     letterSpacing: 10,
                     fontSize: 40,
@@ -97,15 +139,16 @@ class _Settings extends State<Settings> {
                   width: 125,
                   height: 48,
                   decoration: BoxDecoration(
-                      color: Color.fromARGB(50, 217, 217, 217),
+                      color: const Color.fromARGB(50, 217, 217, 217),
                       border: Border.all(
-                        color: Color.fromARGB(50, 217, 217, 217),
+                        color: const Color.fromARGB(50, 217, 217, 217),
                       ),
-                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(20))),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Icon(
+                      const Icon(
                         BootstrapIcons.volume_up_fill,
                         color: Colors.white,
                         size: 30,
@@ -128,15 +171,16 @@ class _Settings extends State<Settings> {
                   width: 125,
                   height: 48,
                   decoration: BoxDecoration(
-                      color: Color.fromARGB(50, 217, 217, 217),
+                      color: const Color.fromARGB(50, 217, 217, 217),
                       border: Border.all(
-                        color: Color.fromARGB(50, 217, 217, 217),
+                        color: const Color.fromARGB(50, 217, 217, 217),
                       ),
-                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(20))),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Icon(
+                      const Icon(
                         BootstrapIcons.phone_vibrate,
                         color: Colors.white,
                         size: 30,
@@ -155,21 +199,80 @@ class _Settings extends State<Settings> {
                     ],
                   ),
                 ),
-                const SizedBox(height: 50),
+                const SizedBox(height: 30),
+                SizedBox(
+                    width: 200,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        icon: const Icon(Icons.person),
+                        iconColor: Colors.white,
+                        fillColor: Colors.white,
+                        labelText: pseudo,
+                        labelStyle: TextStyle(color: Colors.white),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.white, width: 2.0),
+                          borderRadius: BorderRadius.circular(25.0),
+                        ),
+                      ),
+                      style: TextStyle(color: Colors.white),
+                      onChanged: (value) async {
+                        if (pseudo != "") {
+                          newJoueur = false;
+                        }
+                        pseudo = value;
+                      },
+                    )),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (pseudo != "") {
+                      String? id = await _getId();
+                      await Firebase.initializeApp();
+                      if (newJoueur) {
+                        await FirebaseFirestore.instance
+                            .collection('utilisateur')
+                            .doc(id)
+                            .set({
+                          'pseudo': pseudo,
+                          'partieGagne': 0,
+                          'partiePerdu': 0,
+                          'score': 0
+                        });
+                      } else {
+                        await FirebaseFirestore.instance
+                            .collection('utilisateur')
+                            .doc(id)
+                            .update({'pseudo': pseudo});
+                      }
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content:
+                                Text('Changement du nom fait avec succés')),
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content: Text('Mettre un pseudo complet')),
+                      );
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+                const SizedBox(height: 30),
                 Container(
                   width: 220,
                   height: 49,
                   decoration: BoxDecoration(
-                    color: Color(0xff373855),
+                    color: const Color(0xff373855),
                     borderRadius: BorderRadius.circular(10),
                   ),
                   child: Center(
                     child: Theme(
                       data: ThemeData(
-                        canvasColor: Color(0xff373855),
+                        canvasColor: const Color(0xff373855),
                       ),
                       child: DropdownButton<String>(
-                        underline: SizedBox(),
+                        underline: const SizedBox(),
                         value: _selectedOption,
                         iconEnabledColor: Colors.white,
                         style: const TextStyle(color: Colors.white),
