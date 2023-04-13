0a53a4a0cff3d090047f741384a0b2aaef91f68d
ahr <ahr>
2023-04-13 09:50:06 +0200

    modif cell et grille

--- lib/objects/grille.dart
+++ lib/objects/grille.dart
@@ -284,7 +284,6 @@ class Grille {
     // On vérifie que chaque cellule est valide
     for (Cell cell in _listeCells) {
       if (!cell.isCellValid(this)) {
-        debugPrint("La cellule ${cell.toString()} n'est pas valide");
         return false;
       }
     }
